43c6bef703a3bd123494d9b3852a23b665e082a4
Yuheng Su <gipsyh.icu@gmail.com>
2025-01-16 19:07:49 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -33,17 +33,23 @@ fn main() {
         Aig::from_file(&options.model)
     };
     if !aig.outputs.is_empty() {
-        if !options.certify { // not certifying, move outputs to bads
+        if !options.certify {
+            // not certifying, move outputs to bads
             // Move outputs to bads if no bad properties exist
             if aig.bads.is_empty() {
                 aig.bads = std::mem::take(&mut aig.outputs);
-                println!("info: moved {} outputs to bad properties", aig.bads.len());
+                println!(
+                    "Warning: property not found, moved {} outputs to bad properties",
+                    aig.bads.len()
+                );
+            } else {
+                println!("Warning: outputs are ignored");
             }
         }
     }
-    
+
     let origin_aig = aig.clone();
-    if aig.bads.is_empty() { // keep the orginal behaviours
+    if aig.bads.is_empty() {
         println!("warning: no property to be checked");
         verify_certifaiger(&aig, &options);
         exit(20);
@@ -66,7 +72,7 @@ fn main() {
         if options.preprocess.sec {
             panic!("sec not support");
         }
-        
+
         let assert_constrain = matches!(options.engine, options::Engine::IC3);
         let keep_dep = assert_constrain;
         ts = ts.simplify(&[], keep_dep, !assert_constrain);
