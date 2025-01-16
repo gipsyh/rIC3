4941ad275de09f5f048ac35093e9f634adcb0846
Yuheng Su <gipsyh.icu@gmail.com>
2025-01-16 11:37:20 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
-use std::collections::HashMap;
 use aig::Aig;
 use clap::Parser;
 use rIC3::{Options, IC3};
+use std::collections::HashMap;
 use transys::Transys;
 
 fn main() {
     let args = Options::parse();
     let aig = Aig::from_file(&args.model);
     let rst = HashMap::new();
-    let ts = Transys::from_aig(&aig, &rst);
+    let mut ts = Transys::from_aig(&aig, &rst);
+    ts = ts.simplify(&[], true, false);
 
     let mut ic3 = IC3::new(args, ts);
     println!("result: {}", ic3.check_with_int_hanlder());
