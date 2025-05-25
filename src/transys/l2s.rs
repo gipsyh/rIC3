831a6a5529564d13af354f71666d26635e30beb0
Yuheng Su <gipsyh.icu@gmail.com>
2025-05-25 04:04:04 +0000

    update l2s

--- src/transys/l2s.rs
+++ src/transys/l2s.rs
@@ -15,14 +15,21 @@ impl Transys {
         for (l, nl) in self.latch.iter().zip(nls.iter()) {
             eqs.push(l2s.rel.new_xnor(l.lit(), nl.lit()));
         }
-        let eq = l2s
-            .rel
-            .new_and(eqs.into_iter().chain(once(self.justice[0])));
+        let eq = l2s.rel.new_and(eqs);
         let encounter = l2s.new_var();
-        let next = l2s.rel.new_or([eq, encounter.lit()]);
-        l2s.add_latch(encounter, Some(false), next);
+        let encounter_next = l2s.rel.new_or([eq, encounter.lit()]);
+        l2s.add_latch(encounter, Some(false), encounter_next);
+        let mut jls = Vec::new();
+        for &j in self.justice.iter() {
+            let jl = l2s.new_var();
+            jls.push(jl);
+            let jln = l2s.rel.new_and([encounter_next, j]);
+            l2s.add_latch(jl, Some(false), jln);
+        }
         assert!(l2s.fairness.is_empty());
-        l2s.bad = l2s.rel.new_and([eq, encounter.lit()]);
+        l2s.bad = l2s
+            .rel
+            .new_and(jls.into_iter().map(|l| l.lit()).chain(once(eq)));
         l2s.justice.clear();
         l2s.fairness.clear();
         l2s
