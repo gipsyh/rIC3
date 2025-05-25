26c575981b2e1f8a3dc80c4c2d00d1b17bb82f27
Yuheng Su <gipsyh.icu@gmail.com>
2025-05-25 16:22:59 +0000

    update

--- src/transys/l2s.rs
+++ src/transys/l2s.rs
@@ -20,13 +20,12 @@ impl Transys {
         let encounter_next = l2s.rel.new_or([eq, encounter.lit()]);
         l2s.add_latch(encounter, Some(false), encounter_next);
         let mut jls = Vec::new();
-        for &j in self.justice.iter() {
+        for &j in self.justice.iter().chain(self.fairness.iter()) {
             let jl = l2s.new_var();
             jls.push(jl);
             let jln = l2s.rel.new_and([encounter_next, j]);
             l2s.add_latch(jl, Some(false), jln);
         }
-        assert!(l2s.fairness.is_empty());
         l2s.bad = l2s
             .rel
             .new_and(jls.into_iter().map(|l| l.lit()).chain(once(eq)));
