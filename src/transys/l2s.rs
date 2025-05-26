76f1200c6a1603be60ab9796d24461ce27d24e80
Yuheng Su <gipsyh.icu@gmail.com>
2025-05-26 14:26:42 +0000

    update l2s

--- src/transys/l2s.rs
+++ src/transys/l2s.rs
@@ -12,24 +12,25 @@ impl Transys {
             l2s.add_latch(nl, None, nl.lit());
         }
         let mut eqs = Vec::new();
+        let mut eqns = Vec::new();
         for (l, nl) in self.latch.iter().zip(nls.iter()) {
             eqs.push(l2s.rel.new_xnor(l.lit(), nl.lit()));
+            eqns.push(l2s.rel.new_xnor(self.next(l.lit()), nl.lit()));
         }
         let eq = l2s.rel.new_and(eqs);
+        let eqn = l2s.rel.new_and(eqns);
         let encounter = l2s.new_var();
         let encounter_next = l2s.rel.new_or([eq, encounter.lit()]);
         l2s.add_latch(encounter, Some(false), encounter_next);
-        let mut jls = Vec::new();
+        let mut jlns = Vec::new();
         for &j in self.justice.iter().chain(self.fairness.iter()) {
             let jl = l2s.new_var();
-            jls.push(jl);
             let jln = l2s.rel.new_and([encounter_next, j]);
             let jln = l2s.rel.new_or([jl.lit(), jln]);
+            jlns.push(jln);
             l2s.add_latch(jl, Some(false), jln);
         }
-        l2s.bad = l2s
-            .rel
-            .new_and(jls.into_iter().map(|l| l.lit()).chain(once(eq)));
+        l2s.bad = l2s.rel.new_and(jlns.into_iter().chain(once(eqn)));
         l2s.justice.clear();
         l2s.fairness.clear();
         l2s
