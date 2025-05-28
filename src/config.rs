8a7dc50bd98b5c78b4d2d815dfc5013461f07219
Yuheng Su <gipsyh.icu@gmail.com>
2025-05-28 03:49:32 +0000

    remove shadow-rs

--- src/config.rs
+++ src/config.rs
@@ -1,8 +1,6 @@
 use clap::{Args, Parser, ValueEnum};
-use shadow_rs::shadow;
 use std::path::PathBuf;
 
-shadow!(build);
 /// rIC3 model checker
 #[derive(Parser, Debug, Clone)]
 #[command(
@@ -10,7 +8,6 @@ shadow!(build);
     about,
     after_help = "Copyright (C) 2023 - Present, Yuheng Su <gipsyh.icu@gmail.com>. All rights reserved."
 )]
-#[clap(long_version = build::CLAP_LONG_VERSION)]
 pub struct Config {
     /// model checking engine
     #[arg(short, long, value_enum, default_value_t = Engine::Portfolio)]
