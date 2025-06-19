5aa562ae1eda85bf05e44c0995617a80210695ca
Yuheng Su <gipsyh.icu@gmail.com>
2025-06-19 21:30:35 +0800

    enable print unsafe trace of btor model

--- src/frontend/btor/mod.rs
+++ src/frontend/btor/mod.rs
@@ -4,13 +4,14 @@ mod certificate;
 use super::Frontend;
 use crate::{Proof, Witness, config::Config, transys as bl, wl::transys::WlTransys};
 use btor::Btor;
-use giputils::hash::GHashMap;
+use giputils::{bitvec::BitVec, hash::GHashMap};
 use log::{error, warn};
 use logicrs::Var;
 use std::{fmt::Display, path::Path, process::exit};
 
 pub struct BtorFrontend {
     _btor: Btor,
+    owts: WlTransys,
     wts: WlTransys,
     _cfg: Config,
     // wordlevel restore
@@ -42,6 +43,7 @@ impl BtorFrontend {
         let wl_rst = GHashMap::from_iter((0..wts.input.len() + wts.latch.len()).map(|i| (i, i)));
         Self {
             _btor: btor,
+            owts: wts.clone(),
             wts,
             _cfg: cfg.clone(),
             wl_rst,
@@ -62,7 +64,6 @@ impl Frontend for BtorFrontend {
                 .into_iter()
                 .map(|(i, (j, k))| (Var::new(i + 1), (self.wl_rst[&j], k))),
         );
-
         bitblast.lower_to_ts()
     }
 
@@ -70,8 +71,43 @@ impl Frontend for BtorFrontend {
         todo!()
     }
 
-    fn unsafe_certificate(&mut self, _witness: Witness) -> Box<dyn Display> {
-        todo!()
+    fn unsafe_certificate(&mut self, witness: Witness) -> Box<dyn Display> {
+        let mut res = vec!["sat".to_string(), "b0".to_string()];
+        let num_input = self.owts.input.len();
+        let mut init = GHashMap::new();
+        for i in witness.state[0].iter() {
+            let (w, b) = self.bb_rst[&i.var()];
+            let lid = w - num_input;
+            init.entry(lid)
+                .or_insert_with(|| BitVec::new_with(self.owts.latch[lid].sort().bv(), false))
+                .set(b, i.polarity());
+        }
+        if !init.is_empty() {
+            res.push("#0".to_string());
+            let mut init = init.into_iter().collect::<Vec<_>>();
+            init.sort_by_key(|(i, _)| *i);
+            for (i, v) in init.iter() {
+                res.push(format!("{i} {v}"));
+            }
+        }
+        for (t, x) in witness.input.into_iter().enumerate() {
+            let mut input = GHashMap::new();
+            for i in x {
+                let (w, b) = self.bb_rst[&i.var()];
+                input
+                    .entry(w)
+                    .or_insert_with(|| BitVec::new_with(self.owts.input[w].sort().bv(), false))
+                    .set(b, i.polarity());
+            }
+            res.push(format!("@{t}"));
+            let mut input = input.into_iter().collect::<Vec<_>>();
+            input.sort_by_key(|(i, _)| *i);
+            for (i, v) in input.iter() {
+                res.push(format!("{i} {v}"));
+            }
+        }
+        res.push(".\n".to_string());
+        Box::new(res.join("\n"))
     }
 
     fn certify(&mut self, _model: &Path, _cert: &Path) -> bool {
