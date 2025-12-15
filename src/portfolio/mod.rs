5fec3c7b0f7e0ed7113ccabede1e904559e2075e
Yuheng Su <gipsyh.icu@gmail.com>
2025-12-15 21:42:20 +0800

    refactor: simplify portfolio state management and enhance engine monitoring

--- src/portfolio/mod.rs
+++ src/portfolio/mod.rs
@@ -1,43 +1,25 @@
 use crate::EngineConfig;
 use log::{error, info};
-use process_control::{ChildExt, Control};
+use nix::errno::Errno;
+use nix::sys::wait::{WaitStatus, waitpid};
+use nix::unistd::Pid;
+use std::sync::mpsc::Sender;
+use std::thread::JoinHandle;
 use std::{
     collections::HashMap,
     env::current_exe,
     io::{BufRead, BufReader, Read},
     mem::take,
-    ops::{Deref, DerefMut},
     path::PathBuf,
-    process::{Command, Stdio, exit},
-    sync::{Arc, Condvar, Mutex},
-    thread::spawn,
+    process::{Command, Stdio},
+    sync::{
+        Arc,
+        atomic::{AtomicBool, Ordering},
+        mpsc,
+    },
+    thread,
 };
 use tempfile::{NamedTempFile, TempDir};
-
-enum PortfolioState {
-    Checking(usize),
-    Finished(bool, String, Option<NamedTempFile>),
-    Terminate,
-}
-
-impl PortfolioState {
-    fn new(nt: usize) -> Self {
-        PortfolioState::Checking(nt)
-    }
-}
-
-impl PortfolioState {
-    fn is_checking(&self) -> bool {
-        matches!(self, Self::Checking(_))
-    }
-
-    fn result(&mut self) -> (bool, String, Option<NamedTempFile>) {
-        let Self::Finished(res, config, certificate) = self else {
-            panic!()
-        };
-        (*res, config.clone(), take(certificate))
-    }
-}
 
 pub struct Portfolio {
     cert: Option<PathBuf>,
@@ -45,13 +27,27 @@ pub struct Portfolio {
     cfg: EngineConfig,
     engines: Vec<Engine>,
     temp_dir: TempDir,
-    engine_pids: Vec<i32>,
-    state: Arc<(Mutex<PortfolioState>, Condvar)>,
+    engine_pids: Vec<Pid>,
+    stop_flag: Arc<AtomicBool>,
+    monitor: Option<JoinHandle<()>>,
 }
 
 struct Engine {
     name: String,
     cmd: Command,
+}
+
+fn monitor_run(tx: Sender<Option<WaitStatus>>) {
+    loop {
+        match waitpid(None, None) {
+            Ok(status) => {
+                tx.send(Some(status)).unwrap();
+            }
+            Err(Errno::EINTR) => continue,
+            Err(_) => break,
+        }
+    }
+    tx.send(None).unwrap();
 }
 
 impl Portfolio {
@@ -83,173 +79,129 @@ impl Portfolio {
             }
             _ => {
                 error!("Error: unsupported file format");
-                exit(1);
+                panic!();
             }
         };
         for (name, args) in portfolio_config {
             new_engine(name, &args);
         }
-        let ps = PortfolioState::new(engines.len());
         Self {
             cert,
             cfg,
             engines,
             temp_dir,
             engine_pids: Default::default(),
-            state: Arc::new((Mutex::new(ps), Condvar::new())),
+            stop_flag: Arc::new(AtomicBool::new(false)),
+            monitor: None,
         }
     }
 
-    pub fn terminate(&mut self) {
-        let Ok(mut lock) = self.state.0.try_lock() else {
-            return;
-        };
-        if lock.is_checking() {
-            *lock = PortfolioState::Terminate;
-            let pids: Vec<String> = self.engine_pids.iter().map(|p| format!("{}", *p)).collect();
-            let pid = pids.join(",");
-            let _ = Command::new("pkill")
-                .args(["-9", "--parent", &pid])
-                .output();
-            let mut kill = Command::new("kill");
-            kill.arg("-9");
-            for p in pids {
-                kill.arg(p);
-            }
-            let _ = kill.output().unwrap();
-            self.engine_pids.clear();
-            let _ = Command::new("rm")
-                .arg("-rf")
-                .arg(self.temp_dir.path())
-                .output();
+    fn terminate_inner(&mut self) {
+        for pid in self.engine_pids.iter() {
+            let _ = nix::sys::signal::kill(*pid, nix::sys::signal::Signal::SIGTERM);
         }
-        drop(lock);
+        take(&mut self.monitor).unwrap().join().unwrap();
     }
 
-    fn check_inner(&mut self) -> Option<bool> {
-        #[cfg(target_os = "linux")]
-        let wmem = self.cfg.portfolio.wmem_limit * 1024 * 1024 * 1024;
-        let lock = self.state.0.lock().unwrap();
+    pub fn check(&mut self) -> Option<bool> {
+        let mut running = HashMap::new();
         for mut engine in take(&mut self.engines) {
             let certificate = if self.cert.is_some() {
-                let certificate = tempfile::NamedTempFile::new_in(self.temp_dir.path()).unwrap();
+                let certificate = NamedTempFile::new_in(self.temp_dir.path()).unwrap();
                 let certify_path = certificate.path().as_os_str().to_str().unwrap();
                 engine.cmd.arg(certify_path);
                 Some(certificate)
             } else {
                 None
             };
-            let mut child = engine
+
+            let child = engine
                 .cmd
                 .stdout(Stdio::piped())
                 .stderr(Stdio::piped())
                 .spawn()
                 .unwrap();
-            self.engine_pids.push(child.id() as i32);
-            let state = self.state.clone();
-            spawn(move || {
-                let config = engine
-                    .cmd
-                    .get_args()
-                    .skip(1)
-                    .map(|cstr| cstr.to_str().unwrap())
-                    .collect::<Vec<&str>>();
-                let config = config.join(" ");
-                info!("start engine {}", engine.name);
-                let stdout = child.stdout.take().unwrap();
-                #[cfg(target_os = "linux")]
-                let status = child
-                    .controlled()
-                    .memory_limit(wmem)
-                    .wait()
-                    .unwrap()
-                    .unwrap();
-                #[cfg(target_os = "macos")]
-                let status = child.controlled().wait().unwrap().unwrap();
-                let mut res = None;
-                if let Some(0) = status.code() {
-                    let reader = BufReader::new(stdout);
-                    let mut res_line = String::new();
-                    for l in reader.lines().map_while(Result::ok) {
-                        res_line = l;
-                    }
-                    match res_line.as_str() {
-                        "SAT" => res = Some(false),
-                        "UNSAT" => res = Some(true),
-                        _ => (),
-                    }
-                }
-                let Some(res) = res else {
-                    let mut ps = state.0.lock().unwrap();
-                    if let PortfolioState::Checking(np) = ps.deref_mut() {
-                        if !matches!(status.code(), Some(0)) {
-                            info!(
-                                "{} unexpectedly exited, exit code: {:?}",
-                                engine.name,
-                                status.code()
-                            );
+
+            info!("start engine {}", engine.name);
+
+            let pid = Pid::from_raw(child.id() as i32);
+            self.engine_pids.push(pid);
+            running.insert(pid, (child, engine, certificate));
+        }
+
+        let (tx, rx) = mpsc::channel();
+
+        self.monitor = Some(thread::spawn(move || monitor_run(tx)));
+
+        loop {
+            if self.stop_flag.load(Ordering::Relaxed) {
+                info!("Interrupted by external signal");
+                self.terminate_inner();
+                return None;
+            }
+
+            match rx.recv_timeout(std::time::Duration::from_millis(100)) {
+                Ok(msg) => match msg {
+                    Some(WaitStatus::Exited(pid, code)) => {
+                        if let Some((mut child, engine, certificate)) = running.remove(&pid) {
+                            if code == 0 {
+                                let stdout = child.stdout.take().unwrap();
+                                let reader = BufReader::new(stdout);
+                                let mut res = None;
+                                for l in reader.lines().map_while(Result::ok) {
+                                    match l.as_str() {
+                                        "SAT" => res = Some(false),
+                                        "UNSAT" => res = Some(true),
+                                        _ => (),
+                                    }
+                                }
+
+                                if let Some(r) = res {
+                                    let config = engine
+                                        .cmd
+                                        .get_args()
+                                        .skip(1)
+                                        .map(|s| s.to_str().unwrap())
+                                        .collect::<Vec<_>>()
+                                        .join(" ");
+                                    info!("best configuration: {config}");
+                                    if let Some(cert) = &self.cert
+                                        && let Some(c) = certificate
+                                    {
+                                        let _ = std::fs::copy(c.path(), cert);
+                                    }
+                                    self.terminate_inner();
+                                    return Some(r);
+                                }
+                            } else {
+                                let mut stderr = String::new();
+                                if let Some(mut e) = child.stderr.take() {
+                                    let _ = e.read_to_string(&mut stderr);
+                                }
+                                info!("{} exited with code {}: {}", engine.name, code, stderr);
+                            }
                         }
-                        let mut stderr = String::new();
-                        child.stderr.unwrap().read_to_string(&mut stderr).unwrap();
-                        info!("{stderr}");
-                        *np -= 1;
-                        if *np == 0 {
-                            state.1.notify_one();
-                        }
                     }
-                    return;
-                };
-                let mut lock = state.0.lock().unwrap();
-                if lock.is_checking() {
-                    *lock = PortfolioState::Finished(res, config, certificate);
-                    state.1.notify_one();
+                    Some(WaitStatus::Signaled(pid, _, _)) => {
+                        running.remove(&pid);
+                    }
+                    None => {
+                        error!("all workers unexpectedly exited :(");
+                        self.terminate_inner();
+                        return None;
+                    }
+                    _ => unreachable!(),
+                },
+                Err(mpsc::RecvTimeoutError::Timeout) => continue,
+                Err(mpsc::RecvTimeoutError::Disconnected) => {
+                    error!("monitor thread disconnected");
+                    unreachable!();
                 }
-            });
+            }
         }
-        let mut result = self.state.1.wait(lock).unwrap();
-        if let PortfolioState::Checking(np) = result.deref() {
-            assert!(*np == 0);
-            error!("all workers unexpectedly exited :(");
-            return None;
-        }
-        let (res, config, certificate) = result.result();
-        drop(result);
-        info!("best configuration: {config}");
-        if let Some(cert) = &self.cert {
-            std::fs::copy(certificate.as_ref().unwrap(), cert).unwrap();
-        }
-        let pids: Vec<String> = self.engine_pids.iter().map(|p| format!("{}", *p)).collect();
-        let pid = pids.join(",");
-        let _ = Command::new("pkill")
-            .args(["-9", "--parent", &pid])
-            .output();
-        let mut kill = Command::new("kill");
-        kill.arg("-9");
-        for p in pids {
-            kill.arg(p);
-        }
-        let _ = kill.output().unwrap();
-        self.engine_pids.clear();
-        Some(res)
     }
 
-    pub fn check(&mut self) -> Option<bool> {
-        // let ric3 = self as *mut Self as usize;
-        // ctrlc::set_handler(move || {
-        //     let ric3 = unsafe { &mut *(ric3 as *mut Portfolio) };
-        //     ric3.terminate();
-        //     exit(124);
-        // })
-        // .unwrap();
-        self.check_inner()
-    }
-}
-
-impl Drop for Portfolio {
-    fn drop(&mut self) {
-        let _ = Command::new("rm")
-            .arg("-rf")
-            .arg(self.temp_dir.path())
-            .output();
+    pub fn stop_flag(&self) -> Arc<AtomicBool> {
+        self.stop_flag.clone()
     }
 }
