d592b8db404644b8798a9ab9ea2f4ad9789d582a
Yuheng Su <gipsyh.icu@gmail.com>
2026-05-18 06:58:07 +0000

    Refactor fifo module for improved readability and consistency in formatting

--- examples/fifo/fifo.v
+++ examples/fifo/fifo.v
@@ -1,38 +1,39 @@
 module fifo (
     input clk,
     input rst,
-    input wr_en, rd_en,
+    input wr_en,
+    input rd_en,
     input [7:0] data_in,
     output reg [7:0] data_out,
-    output wire empty, full
+    output wire empty,
+    output wire full
 );
 
-    reg [7:0] mem [3:0];
+    reg [7:0] mem[3:0];
     reg [1:0] rd_ptr, wr_ptr;
     reg [2:0] count;
 
     initial rd_ptr = 0;
     initial wr_ptr = 0;
-    initial count  = 0;
+    initial count = 0;
 
     always @(posedge clk) begin
         if (wr_en && !full) begin
             mem[wr_ptr] <= data_in;
             wr_ptr <= wr_ptr + 1;
-            count  = count + 1;
+            count = count + 1;
         end
         if (rd_en && !empty) begin
             data_out <= mem[rd_ptr];
-            rd_ptr <= rd_ptr + 1;
-            count  = count - 1;
+            rd_ptr   <= rd_ptr + 1;
+            count = count - 1;
         end
     end
 
     assign empty = (count == 0);
     assign full  = (count == 4);
-    
-    always @(posedge clk) begin
-        assert((count & 3) == ((wr_ptr - rd_ptr) & 3));
-    end
+
+    p0 :
+    assert property (@(posedge clk) disable iff (!rst_n) (count & 3) == ((wr_ptr - rd_ptr) & 3));
 
 endmodule
