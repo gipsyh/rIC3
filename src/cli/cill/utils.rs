389c4dea48e9c92d97fbd7aa01466592cf174894
Yuheng Su <gipsyh.icu@gmail.com>
2026-05-26 04:41:56 +0000

    refactor: enhance save_trace method to handle optional path and enrich cex

--- src/cli/cill/utils.rs
+++ src/cli/cill/utils.rs
@@ -30,18 +30,26 @@ impl CIll {
                 .cloned()
                 .collect();
             cex = cex.filter(|t| dut_terms.contains(t));
+            if let Some(p) = p {
+                let bwit = self
+                    .dut_bf
+                    .wl_certificate(McWlCertificate::SAT(cex.clone()));
+                fs::write(&p, format!("{}", bwit))?;
+            }
+
+            let vcd_file = BufWriter::new(File::create(&vcd)?);
+            cex.enrich(&self.dut_wsym.keys().cloned().collect());
+            wlwitness_vcd(&cex, &self.dut_wsym, vcd_file, "")?;
+        } else {
+            if let Some(_) = p {
+                todo!();
+            }
+
+            let vcd_file = BufWriter::new(File::create(&vcd)?);
+            cex.enrich(&self.wsym.keys().cloned().collect());
+            wlwitness_vcd(&cex, &self.wsym, vcd_file, "")?;
         }
 
-        if let Some(p) = p {
-            let bwit = self
-                .dut_bf
-                .wl_certificate(McWlCertificate::SAT(cex.clone()));
-            fs::write(&p, format!("{}", bwit))?;
-        }
-
-        let vcd_file = BufWriter::new(File::create(&vcd)?);
-        cex.enrich(&self.dut_wsym.keys().cloned().collect());
-        wlwitness_vcd(&cex, &self.dut_wsym, vcd_file, "")?;
         Ok(())
     }
 }
