ef278d840bd0ee7daf0c80e9f58c0b2d0f3a3279
Yuheng Su <gipsyh.icu@gmail.com>
2026-05-26 15:24:02 +0000

    update

--- src/cli/cill/mod.rs
+++ src/cli/cill/mod.rs
@@ -19,6 +19,7 @@ use btor::Btor;
 use clap::Subcommand;
 use giputils::{file::remove_if_exists, logger::with_log_level};
 use log::LevelFilter;
+use logicrs::fol::term_gc;
 use rIC3::{
     frontend::{Frontend, btor::BtorFrontend},
     transys::{Transys, certify::Restore},
@@ -108,6 +109,7 @@ impl CIll {
             .to_file(rp.path("cill/candinv/linked.btor"));
 
         let (mut ts, bb_map) = wts.bitblast_to_ts();
+        term_gc();
         let ots = ts.clone();
         let mut ts_rst = Restore::new(&ts);
         with_log_level(LevelFilter::Warn, || ts.simplify(&mut ts_rst));
