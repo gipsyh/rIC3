fc3ad60f96b6206976fa4a4600d0c0b4f872e733
Yuheng Su <gipsyh.icu@gmail.com>
2026-06-27 15:43:44 +0800

    fix bug

--- src/wltransys/mod.rs
+++ src/wltransys/mod.rs
@@ -99,7 +99,7 @@ impl WlTransys {
     }
 
     pub fn compress_constraints(&mut self) {
-        if self.bad.len() <= 1 {
+        if self.constraint.len() <= 1 {
             return;
         }
         let constraint = take(&mut self.constraint);
@@ -121,36 +121,3 @@ impl WlTransys {
     }
 }
 
-//     pub fn term_next(&self, term: &Term) -> Term {
-//         match term.deref() {
-//             TermType::Const(_) => term.clone(),
-//             TermType::Var(_) => self.latch_next[term].clone(),
-//             TermType::UniOp(op) => {
-//                 let a = self.term_next(&op.a);
-//                 a.uniop(op.ty)
-//             }
-//             TermType::BiOp(op) => {
-//                 let a = self.term_next(&op.a);
-//                 let b = self.term_next(&op.b);
-//                 a.biop(&b, op.ty)
-//             }
-//             TermType::TriOp(_) => todo!(),
-//             TermType::ExtOp(op) => {
-//                 let a = self.term_next(&op.a);
-//                 a.extop(op.ty, op.length)
-//             }
-//             TermType::SliceOp(op) => {
-//                 let a = self.term_next(&op.a);
-//                 a.slice(op.upper, op.lower)
-//             }
-//         }
-//     }
-
-//     pub fn term_cube_next(&self, cube: &[Term]) -> TermCube {
-//         let mut next = TermCube::new();
-//         for l in cube.iter() {
-//             next.push(self.term_next(l));
-//         }
-//         next
-//     }
-// }
